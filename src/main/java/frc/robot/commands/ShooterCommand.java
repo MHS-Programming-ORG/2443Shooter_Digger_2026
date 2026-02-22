056cd2cfa999aeed208dd5e0c52819d06f6974ae
John Carlo Villanueva <johncarlovillanueva2008@gmail.com>
2026-02-21 15:37:46 -1000

    package changed

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -1,7 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.shoooterSubsystems.ShooterSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShooterCommand extends Command {
   private ShooterSubsystem shooterSub;
